9e0cda2159047867755a61cf1d66dc3ea1caf532
Bill Zhu <bill.zhu@airwallex.com>
2017-12-28 10:50:59 +0800

    commit

--- service/accessToken.js
+++ service/accessToken.js
@@ -9,8 +9,6 @@ const getAccessToken = async () => {
 
 //去微信服务端获取access_token
 toWechatAccessToken = async () => {
-    let appid = "wxe2688e3d857ef00f"
-    let secret = "740fa80d6ee2d8b4702eb0545a2eb82c"
     var res = await request(`http://47.91.245.123:8081/getAccessToken`)
     console.log(res.body);
     return Promise.resolve(res.body);
