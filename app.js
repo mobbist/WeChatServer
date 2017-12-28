e72c67f8c8dd9ec27dacc9ec111f09e22e6cbb30
Bill Zhu <bill.zhu@airwallex.com>
2017-12-28 16:28:42 +0800

    c

--- app.js
+++ app.js
@@ -2,14 +2,59 @@ const Koa = require("koa");
 const static = require("koa-static");
 const path = require("path");
 const app = new Koa();
-const bodyParser = require("koa-bodyparser");
-app.use(bodyParser())
+
+// xmlTool.js
+const xml2js = require('xml2js')
+
+xmlToJson = (str) => {
+    return new Promise((resolve, reject) => {
+        const parseString = xml2js.parseString
+        parseString(str, (err, result) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(result)
+            }
+        })
+    })
+}
+
+jsonToXml = (obj) => {
+    const builder = new xml2js.Builder()
+    return builder.buildObject(obj)
+}
+
+
 app.use(async (ctx, next) => {
-    if (ctx.method === 'POST') {
-        console.log(ctx.request.body);
+    if (ctx.method == 'POST' && ctx.is('text/xml')) {
+        let promise = new Promise(function (resolve, reject) {
+            let buf = ''
+            ctx.req.setEncoding('utf8')
+            ctx.req.on('data', (chunk) => {
+                buf += chunk
+            })
+            ctx.req.on('end', () => {
+                xml.xmlToJson(buf)
+                    .then(resolve)
+                    .catch(reject)
+            })
+        })
+
+        await promise.then((result) => {
+            ctx.req.body = result
+        })
+            .catch((e) => {
+                e.status = 400
+            })
+
+        next()
+    } else {
+        await next()
     }
-    await next();
 })
+
+
+
 
 
 
