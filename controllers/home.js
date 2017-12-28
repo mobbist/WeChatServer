e705a46cf7a6e7b3e00f07eba251d1007aa6d91d
Bill Zhu <bill.zhu@airwallex.com>
2017-12-28 17:27:48 +0800

    c

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,5 @@
 //控制器, 负责业务调度
-const { easyCheck } = require("../service/homeService");
+const { checkSignature } = require("../service/homeService");
 //const bodyParser = require("koa-bodyparser");
 const { getAccessToken } = require("../service/accessToken");
 
