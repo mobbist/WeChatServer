8d9bf1d1729b3d48207702b79af9252ce296604f
Bill Zhu <bill.zhu@airwallex.com>
2017-12-28 17:55:38 +0800

    c

--- app.js
+++ app.js
@@ -9,7 +9,19 @@ const jsonToXml = (obj) => {
     const builder = new xml2js.Builder()
     return builder.buildObject(obj)
 }
-
+message = {
+    text(msg, content) {
+        return xml.jsonToXml({
+            xml: {
+                ToUserName: msg.FromUserName,
+                FromUserName: msg.ToUserName,
+                CreateTime: Date.now(),
+                MsgType: msg.MsgType,
+                Content: content
+            }
+        })
+    }
+}
 
 app.use(async (ctx, next) => {
     //这里监听微信消息
@@ -23,7 +35,9 @@ app.use(async (ctx, next) => {
         //接受完成转为JSON
         ctx.req.on('end', () => {
             xml2js.parseString(buf, (err, result) => {
-                console.log(result)
+                let res = message.text(result.xml, result.xml.Content)
+                ctx.res.setHeader('Content-Type', 'application/xml')
+                ctx.res.end(res)
                 // if (err) {
                 //     Promise.reject(err)
                 // } else {
