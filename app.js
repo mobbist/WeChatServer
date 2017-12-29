efb00bd23a3117785a482c250616ce1cf8df4d35
Bill Zhu <bill.zhu@airwallex.com>
2017-12-29 10:08:39 +0800

    commit

--- app.js
+++ app.js
@@ -3,61 +3,6 @@ const static = require("koa-static");
 const path = require("path");
 const app = new Koa();
 
-// xmlTool.js
-const xml2js = require('xml2js')
-const jsonToXml = (obj) => {
-    const builder = new xml2js.Builder()
-    return builder.buildObject(obj)
-}
-message = {
-    text(msg, content) {
-        return jsonToXml({
-            xml: {
-                ToUserName: msg.FromUserName,
-                FromUserName: msg.ToUserName,
-                CreateTime: Date.now(),
-                MsgType: msg.MsgType,
-                Content: content
-            }
-        })
-    }
-}
-
-app.use(async (ctx, next) => {
-    //这里监听微信消息
-    if (ctx.method == 'POST' && ctx.is('text/xml')) {
-        let buf = ''
-        ctx.req.setEncoding('utf8')
-        //监听接受xml数据
-        ctx.req.on('data', (chunk) => {
-            buf += chunk
-        })
-        //接受完成转为JSON
-        ctx.req.on('end', () => {
-
-
-            xml2js.parseString(buf, (err, result) => {
-
-                ctx.res.setHeader('Content-Type', 'application/xml')
-                let res = message.text(result.xml, result.xml.Content)
-
-                ctx.body = res
-                // if (err) {
-                //     Promise.reject(err)
-                // } else {
-                //     Promise.resolve(result)
-                // }
-            })
-            //console.log(json);
-        })
-
-
-
-        await next()
-    } else {
-        ctx.body = "body";
-    }
-})
 
 
 
@@ -65,16 +10,17 @@ app.use(async (ctx, next) => {
 
 
 
-// //加载汇总路由
-// const routers = require("./router/index");
-// app.use(routers.routes()).use(routers.allowedMethods())
+
+//加载汇总路由
+const routers = require("./router/index");
+app.use(routers.routes()).use(routers.allowedMethods())
 
 
 // 静态资源目录对于相对入口文件index.js的路径
-// const staticPath = './static'
-// app.use(static(
-//     path.join(__dirname, staticPath)
-// ))
+const staticPath = './static'
+app.use(static(
+    path.join(__dirname, staticPath)
+))
 
 
 
--- controllers/home.js
+++ controllers/home.js
@@ -2,14 +2,77 @@
 const { checkSignature } = require("../service/homeService");
 //const bodyParser = require("koa-bodyparser");
 const { getAccessToken } = require("../service/accessToken");
-
-async function getIndex(ctx) {
+const xml2js = require('xml2js')
+exports.getIndex = async (ctx) => {
     // let accessToken = await getAccessToken();
+    checkXMLMiddleware(ctx);
 
-
-
-    //ctx.body = "123"
+    // ctx.res.setHeader('Content-Type', 'application/xml')
+    // ctx.res.end("<abc>123</abc>")
 }
-module.exports = {
-    getIndex
+
+//检测是否是XML中间件的请求
+async function checkXMLMiddleware(ctx) {
+    console.log(ctx.is("'application/xml"));
+
+    if (ctx.method == 'POST' && ctx.is('text/xml')) {
+        let buf = ''
+        ctx.req.setEncoding('utf8')
+        //监听接受xml数据
+        ctx.req.on('data', (chunk) => {
+            buf += chunk
+        })
+        //接受完成转为JSON
+        ctx.req.on('end', () => {
+            console.log(buf);
+            // xml2js.parseString(buf, (err, result) => {
+            //     ctx.res.setHeader('Content-Type', 'application/xml')
+            //     let res = message.text(result.xml, result.xml.Content)
+            //     ctx.body = res
+        })
+    }
 }
+// xmlTool.js
+
+// const jsonToXml = (obj) => {
+//     const builder = new xml2js.Builder()
+//     return builder.buildObject(obj)
+// }
+// message = {
+//     text(msg, content) {
+//         return jsonToXml({
+//             xml: {
+//                 ToUserName: msg.FromUserName,
+//                 FromUserName: msg.ToUserName,
+//                 CreateTime: Date.now(),
+//                 MsgType: msg.MsgType,
+//                 Content: content
+//             }
+//         })
+//     }
+// }
+
+// app.use(async (ctx, next) => {
+//     //这里监听微信消息
+
+
+
+//             
+//                 // if (err) {
+//                 //     Promise.reject(err)
+//                 // } else {
+//                 //     Promise.resolve(result)
+//                 // }
+//             })
+//             //console.log(json);
+//         })
+
+
+
+//         await next()
+//     } else {
+//         ctx.body = "body";
+//     }
+// })
+
+
